fd7e8bd8accc3a8588a22e8376423b6c5711cb5b
lucifer9735 <lucifer9735@gmail.com>
2018-05-28 11:09:01 +0800

    v1

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-AutoSnake
+Title: AutoSnake
+Date: 2018-5-28 9:07
+Tags: autosnake
+Category:
+Slug: autosnake
+
+<!-- PELICAN_END_SUMMARY -->
+
+---
+
+**v1** 仅用BFS求最短路径版贪吃蛇
+
+​	平均存活时间12s
+
+​	“目光短浅”
 
 ---
 
--- autosnake.py
+++ autosnake.py
@@ -5,7 +5,7 @@ HEIGHT = 10
 WIDTH = 20
 FIELD_SIZE = HEIGHT * WIDTH
 
-SPEED = 300
+SPEED = 50
 food = 3 * WIDTH + 3
 key = 1 
 score = 1
@@ -48,7 +48,7 @@ def new_food():
         h = randint(1, HEIGHT-2)
         food = h*WIDTH + w
         cell_free = is_cell_free(food, snake, snake_size)
-    win.addch(food//WIDTH, food%WIDTH, '@')
+    win.addch(food//WIDTH, food%WIDTH, '$')
 
 def make_move(pbest_move):
     global key, board, snake, snake_size, score
@@ -56,7 +56,7 @@ def make_move(pbest_move):
     snake[HEAD] += pbest_move
 
     p = snake[HEAD]
-    win.addch(p//WIDTH, p%WIDTH, '*')
+    win.addch(p//WIDTH, p%WIDTH, '@')
 
     if snake[HEAD] == food:
         board[snake[HEAD]] = SNAKE
@@ -68,6 +68,59 @@ def make_move(pbest_move):
         board[snake[snake_size]] = UNDEFINED
         win.addch(snake[snake_size]//WIDTH, snake[snake_size]%WIDTH, ' ')
 
+# autosnake
+def is_movable(idx, move):
+    flag = False
+    if move == LEFT:
+        flag = True if idx%WIDTH > 1 else False
+    elif move == RIGHT:
+        flag = True if idx%WIDTH < (WIDTH-2) else False
+    elif move == UP:
+        flag = True if idx//WIDTH > 1 else False
+    elif move == DOWN:
+        flag = True if idx//WIDTH < (HEIGHT-2) else False
+    return flag
+
+def board_reset(pboard, psnake, psize):
+    for i in range(FIELD_SIZE):
+        if i == food:
+            pboard[i] = FOOD
+        elif is_cell_free(i, psnake, psize):
+            pboard[i] = UNDEFINED
+        else:
+            pboard[i] = SNAKE
+
+def board_scan(pboard, psnake, pfood):
+    queue = []
+    queue.append(pfood)
+    inqueue = [0] * FIELD_SIZE
+    found = False
+    while len(queue) != 0:
+        idx = queue.pop(0)
+        if inqueue[idx] == 1: continue
+        inqueue[idx] = 1
+        for i in range(4):
+            if is_movable(idx, mov[i]):
+                pidx = idx + mov[i]
+                if pidx == psnake[HEAD]:
+                    found = True
+                elif pboard[pidx] < SNAKE:
+                    if pboard[pidx] > pboard[idx]+1:
+                        pboard[pidx] = pboard[idx] + 1
+                    if inqueue[pidx] == 0:
+                        queue.append(pidx)
+    return found
+
+def choose_shortest(pboard, psnake):
+    best_move = ERR
+    min = SNAKE
+    for i in range(4):
+        pidx = psnake[HEAD] + mov[i]
+        if is_movable(psnake[HEAD], mov[i]) and pboard[pidx]<min:
+            min = pboard[pidx]
+            best_move = mov[i]
+    return best_move
+
 
 curses.initscr()
 win = curses.newwin(HEIGHT, WIDTH, 0, 0)
@@ -76,7 +129,7 @@ curses.noecho()
 curses.curs_set(0)
 win.border(0)
 win.nodelay(1)
-win.addch(food//WIDTH, food%WIDTH, '@')
+win.addch(food//WIDTH, food%WIDTH, '$')
 
 
 event = 0
@@ -84,17 +137,26 @@ while event != 27:
     win.addstr(0, 2, ' S:' + str(score) + ' ')
     win.timeout(SPEED)
     event = win.getch()
-    if event != -1:
-        if event == 27:break
-        elif event == h:
-            key = key if key == RIGHT else LEFT
-        elif event == l:
-            key = key if key == LEFT else RIGHT
-        elif event == j:
-            key = key if key == UP else DOWN
-        elif event == k:
-            key = key if key == DOWN else UP
-    make_move(key)
+#    if event != -1:
+#        if event == 27:break
+#        elif event == h:
+#            key = key if key == RIGHT else LEFT
+#        elif event == l:
+#            key = key if key == LEFT else RIGHT
+#        elif event == j:
+#            key = key if key == UP else DOWN
+#        elif event == k:
+#            key = key if key == DOWN else UP
+#    make_move(key)
+    key = key if event == -1 else evet
+    board_reset(board, snake, snake_size)
+    if board_scan(board, snake, food):
+        best_move = choose_shortest(board, snake)
+
+    if best_move != ERR: make_move(best_move)
+    else:
+        print("error!")
+        break
 
 win.keypad(0)
 curses.echo()
